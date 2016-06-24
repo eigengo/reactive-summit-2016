307ce5119ed1b08b02de3d91ee94257145ce5070
Jan Machacek <jan.machacek@gmail.com>
2016-06-24 16:30:34 +0100

    Compatible and comments

--- ingest/src/test/scala/org/eigengo/rsa/captioning/PropertyTest.scala
+++ ingest/src/test/scala/org/eigengo/rsa/captioning/PropertyTest.scala
@@ -6,8 +6,7 @@ import org.scalatest.FlatSpec
 class PropertyTest extends FlatSpec with ProtobufMatchers {
 
   "Generated code should" should "foo" in {
-    v100.Caption("a", 1) should be (forwardCompatibleWith(v101.Caption))
-    v101.Caption("a", 1) should be (backwardCompatibleWith(v100.Caption))
+    v100.Caption("a", 1) should be (compatibleWith(v101.Caption))
   }
 
 }
--- protobuf-testkit/src/main/scala/org/eigengo/protobufcheck/ProtobufMatchers.scala
+++ protobuf-testkit/src/main/scala/org/eigengo/protobufcheck/ProtobufMatchers.scala
@@ -7,37 +7,50 @@ import com.trueaccord.scalapb.{GeneratedMessage, GeneratedMessageCompanion, Mess
 import org.scalatest.Matchers
 import org.scalatest.matchers.{BeMatcher, MatchResult}
 
-
+/**
+  * Defines matchers to be used with the ``be`` verb and ScalaPB-generated code. Its matchers
+  * provide means to verify that the messages are compatible with each other.
+  */
 trait ProtobufMatchers extends Matchers {
 
   private type Right = GeneratedMessageCompanion[_ <: GeneratedMessage with Message[_]]
 
+  /**
+    * Common matcher that matches ``GeneratedMessage`` on the left with the ``GeneratedMessage``
+    * constructed using the ``GeneratedMessageCompanion`` on the right
+    *
+    * @param right the right companion
+    */
   private class CompatibleMatcher(right: Right) extends BeMatcher[GeneratedMessage] {
 
-    private def fdEquals(a: FieldDescriptor)(b: FieldDescriptor): Boolean = {
+    import collection.JavaConversions._
+
+    /**
+      * Returns ``true`` if the two ``FieldDescriptor``s are equal
+      * @param a the first FD
+      * @param b the second FD
+      * @return a == b
+      */
+    protected final def fdEquals(a: FieldDescriptor)(b: FieldDescriptor): Boolean = {
       a.getName == b.getName && a.getType.name() == b.getType.name()
     }
-
-    import collection.JavaConversions._
 
     override def apply(left: GeneratedMessage): MatchResult = {
       val os = new ByteArrayOutputStream()
       left.writeTo(os)
 
-      val inFields = left.companion.descriptor.getFields.toList
-      val outFields = right.descriptor.getFields.toList
-      val pairs = inFields.flatMap { inD =>
-        outFields.find(fdEquals(inD)).map { outD => (inD, outD) }
-      }
+      val leftFields  = left.companion.descriptor.getFields.toList
+      val rightFields = right.descriptor.getFields.toList
+      val intersectedFields = leftFields.flatMap(l => rightFields.find(fdEquals(l)).map(r => (l, r)))
 
-      val outM = right.parseFrom(new ByteArrayInputStream(os.toByteArray))
+      val parsedRight = right.parseFrom(new ByteArrayInputStream(os.toByteArray))
 
-      val failures = pairs.foldLeft(List.empty[(FieldDescriptor, FieldDescriptor, Any, Any)]) {
-        case (r, (inD, outD)) =>
-          val inV = left.getField(inD)
-          val outV = outM.getField(outD)
-          if (!inV.equals(outV)) {
-            (inD, outD, inV, outV) :: r
+      val failures = intersectedFields.foldLeft(List.empty[(FieldDescriptor, FieldDescriptor, Any, Any)]) {
+        case (r, (leftDescriptor, rightDescriptor)) =>
+          val leftValue  = left.getField(leftDescriptor)
+          val rightValue = parsedRight.getField(rightDescriptor)
+          if (!leftValue.equals(rightValue)) {
+            (leftDescriptor, rightDescriptor, leftValue, rightValue) :: r
           } else r
       }
 
@@ -49,8 +62,6 @@ trait ProtobufMatchers extends Matchers {
     }
   }
 
-  def forwardCompatibleWith(right: Right): BeMatcher[GeneratedMessage] = new CompatibleMatcher(right)
-
-  def backwardCompatibleWith(right: Right): BeMatcher[GeneratedMessage] = new CompatibleMatcher(right)
+  def compatibleWith(right: Right): BeMatcher[GeneratedMessage] = new CompatibleMatcher(right)
 
 }
