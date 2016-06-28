fe5a400776d7e794338129d600dd7a8e6c659e43
Jan Machacek <jan.machacek@gmail.com>
2016-06-28 16:06:38 +0100

    Generator

--- ingest/src/test/scala/org/eigengo/rsa/captioning/PropertyTest.scala
+++ ingest/src/test/scala/org/eigengo/rsa/captioning/PropertyTest.scala
@@ -2,8 +2,9 @@ package org.eigengo.rsa.captioning
 
 import org.eigengo.protobufcheck.ProtobufMatchers
 import org.scalatest.FlatSpec
+import org.scalatest.prop.GeneratorDrivenPropertyChecks
 
-class PropertyTest extends FlatSpec with ProtobufMatchers {
+class PropertyTest extends FlatSpec with ProtobufMatchers with GeneratorDrivenPropertyChecks {
 
   "Generated code should" should "foo" in {
     v100.Caption("a", 1) should be (compatibleWith(v101.Caption))
