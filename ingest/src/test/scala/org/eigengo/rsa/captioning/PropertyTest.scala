2541205e598611f546f625f3f276faaf53602a30
Jan Machacek <jan.machacek@gmail.com>
2016-07-03 16:00:26 +0100

    Latest

--- ingest/src/test/scala/org/eigengo/rsa/captioning/PropertyTest.scala
+++ ingest/src/test/scala/org/eigengo/rsa/captioning/PropertyTest.scala
@@ -10,8 +10,6 @@ class PropertyTest extends FlatSpec with ProtobufMatchers with PropertyChecks {
     v100.Caption("a", 1) should be (compatibleWith(v101.Caption))
     v101.Caption("a", 1, Some(true)) should be (compatibleWith(v100.Caption))
 
-    forAll(ProtobufGen.message(v200.Caption))(println)
-
     forAll(ProtobufGen.message(v101.Caption))(_ should be (compatibleWith(v100.Caption)))
   }
 
