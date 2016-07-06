a9ab4cfec0f2333b4f9d4bbe7746b367712a612a
Jan Machacek <jan.machacek@gmail.com>
2016-07-06 13:40:51 +0100

    Json

--- ingest/src/main/scala/org/eigengo/rsa/ScalaPBMarshalling.scala
+++ ingest/src/main/scala/org/eigengo/rsa/ScalaPBMarshalling.scala
@@ -1,13 +1,21 @@
 package org.eigengo.rsa
 
 import akka.http.scaladsl.marshalling.{Marshaller, ToEntityMarshaller}
-import akka.http.scaladsl.model.{ContentTypes, HttpEntity}
-import com.trueaccord.scalapb.GeneratedMessage
+import akka.http.scaladsl.model.MediaType.Compressible
+import akka.http.scaladsl.model.{ContentType, ContentTypes, HttpEntity, MediaType}
+import akka.http.scaladsl.unmarshalling.{FromEntityUnmarshaller, Unmarshaller}
 import com.trueaccord.scalapb.json.JsonFormat
+import com.trueaccord.scalapb.{GeneratedMessage, GeneratedMessageCompanion, Message}
 
 trait ScalaPBMarshalling {
 
-  implicit def protobufDerivedMarshaller[U <: GeneratedMessage]: ToEntityMarshaller[U] = {
+  def scalaPBFromRequestUnmarshaller[O <: GeneratedMessage with Message[O]](companion: GeneratedMessageCompanion[O]): FromEntityUnmarshaller[O] = {
+
+
+    Unmarshaller.firstOf()
+  }
+
+  implicit def scalaPBToEntityMarshaller[U <: GeneratedMessage]: ToEntityMarshaller[U] = {
     def jsonMarshaller(): ToEntityMarshaller[U] = {
       val contentType = ContentTypes.`application/json`
       Marshaller.withFixedContentType(contentType) { value ⇒
@@ -15,7 +23,14 @@ trait ScalaPBMarshalling {
       }
     }
 
-    Marshaller.oneOf(jsonMarshaller())
+    def protobufMarshaller(): ToEntityMarshaller[U] = {
+      val contentType = ContentType(MediaType.applicationBinary("octet-stream", Compressible, "proto"))
+      Marshaller.withFixedContentType(contentType) { value ⇒
+        HttpEntity(contentType, value.toByteArray)
+      }
+    }
+
+    Marshaller.oneOf(jsonMarshaller(), protobufMarshaller())
   }
 
 }
--- ingest/src/test/scala/org/eigengo/rsa/captioning/PropertyTest.scala
+++ ingest/src/test/scala/org/eigengo/rsa/captioning/PropertyTest.scala
@@ -1,16 +1,32 @@
 package org.eigengo.rsa.captioning
 
 import akka.http.scaladsl.marshalling.{Marshalling, ToEntityMarshaller}
+import akka.http.scaladsl.model.{ContentType, ContentTypes, MessageEntity}
 import org.eigengo.protobufcheck.{ProtobufGen, ProtobufMatchers}
 import org.eigengo.rsa.ScalaPBMarshalling
 import org.eigengo.rsa.captioning.v200.Caption.Item.Kind
 import org.scalatest.FlatSpec
 import org.scalatest.prop.PropertyChecks
 
-import scala.concurrent.Await
 import scala.concurrent.duration.Duration
+import scala.concurrent.{Await, Future}
 
 class PropertyTest extends FlatSpec with ProtobufMatchers with PropertyChecks with ScalaPBMarshalling {
+
+  def awaitMarshal[A](value: A, contentType: ContentType)(implicit marshaller: ToEntityMarshaller[A]): MessageEntity = {
+    import scala.concurrent.ExecutionContext.Implicits.global
+
+    val fme = marshaller.apply(value).flatMap { marshallers ⇒
+      marshallers.flatMap {
+        case Marshalling.WithFixedContentType(`contentType`, m) ⇒ Some(m())
+        case _ ⇒ None
+      } match {
+        case h::Nil ⇒ Future.successful(h)
+        case _ ⇒ Future.failed(new RuntimeException(":("))
+      }
+    }
+    Await.result(fme, Duration.Inf)
+  }
 
   "Same major versions" should "be compatible with each other" in {
     v100.Caption("a", 1) should be (compatibleWith(v101.Caption))
@@ -20,20 +36,14 @@ class PropertyTest extends FlatSpec with ProtobufMatchers with PropertyChecks wi
   }
 
   "Marshalling" should "work" in {
-    import scala.concurrent.ExecutionContext.Implicits.global
-
     val x = v200.Caption(
       text = "Hello,",
       ints = Seq(1, 2, 3),
       items = Seq(v200.Caption.Item(accuracy = 1, kind = Kind.Category("foo")), v200.Caption.Item(accuracy = 1, kind = Kind.NamedPerson("bar"))),
       corpus = v200.Caption.Corpus.UNIVERSAL)
-    
-    val v200CaptionMarshaller = implicitly[ToEntityMarshaller[v200.Caption]]
-    Await.result(v200CaptionMarshaller.apply(x).map(_.head), Duration.Inf) match {
-      case Marshalling.WithFixedContentType(_, m) ⇒
-        Console.println(m())
-      case _ ⇒ fail("Bad match")
-    }
+
+    println(awaitMarshal(x, ContentTypes.`application/json`))
+    println(awaitMarshal(x, ContentTypes.`application/octet-stream`))
   }
 
 }
