9fc3a950ad66b9149558fe8bf6e11c7a2608b955
Jan Machacek <jan.machacek@gmail.com>
2016-07-14 16:51:28 +0100

    Linter

--- linter-plugin/src/main/scala/org/eigengo/rsa/linter/MarshallerLinterPlugin.scala
+++ linter-plugin/src/main/scala/org/eigengo/rsa/linter/MarshallerLinterPlugin.scala
@@ -30,6 +30,8 @@ class MarshallerLinterPlugin(val global: Global) extends Plugin {
       override def apply(unit: CompilationUnit): Unit = {
         // the permit
         val permitAnnotationType = rootMirror.getClassIfDefined("org.eigengo.rsa.ScalaPBMarshalling.permit").tpe
+        // the rejected types; note that they are the erased types, because we don't really want to
+        // or need to mess around with variance; checking the erased types is sufficient in this plugin
         val rejectedRhsTypes = List("akka.http.scaladsl.marshalling.Marshaller", "akka.http.scaladsl.unmarshalling.Unmarshaller")
           .map(name ⇒ rootMirror.getClassIfDefined(name).tpe.erasure)
 
@@ -39,23 +41,32 @@ class MarshallerLinterPlugin(val global: Global) extends Plugin {
             .flatMap(t ⇒ Iterator(t) ++ t.children.iterator.flatMap(allTrees))
 
         // checks that the permit annotation is present on the given ``symbol``.
-        def hasPermitAnnotation(symbol: global.Symbol): Boolean = {
+        def hasPermitAnnotation(symbol: global.Symbol): Boolean =
           Option(symbol).forall(_.annotations.exists(_.tpe <:< permitAnnotationType))
-        }
 
-
+        // for now, rejection is the type name that we reject
         type Rejection = String
-
         // checks the tree for disallowed type
         def rejectHandRolled(tree: Tree): Option[Rejection] = {
           if (tree.tpe <:< definitions.NullTpe) None
           else rejectedRhsTypes.find(rejectedType ⇒ tree.tpe.dealiasWiden.erasure <:< rejectedType).map(_.toString())
+          //                                                  ^      ^     ^
+          // note the checks are done using the de-aliased, widened and erased types
         }
 
-        // check all expanded trees of each compilation unit
+        // check all expanded trees of each compilation unit; find
+        //
+        // val x = `rhs`
+        // val x: Tpt = `rhs`
+        // implicit def x = `rhs`
+        // implicit def x: Tpt = `rhs`
+        //
+        // Check the explicit types or the type of the RHS and reject those that are listed in
+        // ``rejectedRhsTypes``.
+        //
         allTrees(unit.body).foreach {
-          case d@ValDef(mods, _, _, rhs) if !hasPermitAnnotation(rhs.symbol) ⇒
-            rejectHandRolled(rhs).foreach { rejection ⇒
+          case d@ValDef(mods, _, tpt, rhs) if !hasPermitAnnotation(rhs.symbol) ⇒
+            rejectHandRolled(rhs).orElse(rejectHandRolled(tpt)).foreach { rejection ⇒
               global.globalError(d.pos, s"Cannot hand-roll val of type $rejection.")
             }
           case d@DefDef(mods, _, _, _, tpt, rhs) if mods.isImplicit && !hasPermitAnnotation(d.symbol) ⇒
