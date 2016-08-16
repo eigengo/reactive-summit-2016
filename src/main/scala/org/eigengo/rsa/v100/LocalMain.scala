3a50309801ac1865e100ec8b5474af685c1fb006
Jan Machacek <jan.machacek@gmail.com>
2016-08-16 17:28:27 +0100

    Latest

--- src/main/scala/org/eigengo/rsa/v100/LocalMain.scala
+++ src/main/scala/org/eigengo/rsa/v100/LocalMain.scala
@@ -6,7 +6,7 @@ object LocalMain {
     System.setProperty("KAFKA_BOOTSTRAP_SERVERS", "localhost:9092")
     // TODO: Cassandra contact points
 
-    org.eigengo.rsa.identity.v100.Main.main(args)
+    //org.eigengo.rsa.identity.v100.Main.main(args)
     org.eigengo.rsa.scene.v100.Main.main(args)
     org.eigengo.rsa.dashboard.v100.Main.main(args)
     org.eigengo.rsa.it.v100.Main.main(args)
