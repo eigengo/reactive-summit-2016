ce2c8248df547ecc59f36e419d8a29fd8d643f1d
Jan Machacek <jan.machacek@gmail.com>
2016-10-12 09:31:46 +0100

    x

--- vision-text/src/main/java/org/eigengo/rsa/text/v100/TextServiceImpl.java
+++ vision-text/src/main/java/org/eigengo/rsa/text/v100/TextServiceImpl.java
@@ -19,35 +19,38 @@
 package org.eigengo.rsa.text.v100;
 
 import akka.Done;
-import akka.actor.Cancellable;
 import akka.japi.Pair;
 import akka.stream.Materializer;
+import akka.stream.OverflowStrategy;
 import akka.stream.javadsl.Flow;
+import akka.stream.javadsl.Sink;
 import akka.stream.javadsl.Source;
+import akka.stream.javadsl.SourceQueueWithComplete;
 import com.google.inject.Inject;
 import com.lightbend.lagom.javadsl.api.broker.Topic;
 import com.lightbend.lagom.javadsl.broker.TopicProducer;
-import com.lightbend.lagom.javadsl.persistence.Offset;
 import org.eigengo.rsa.Envelope;
-import scala.concurrent.duration.FiniteDuration;
-
-import java.util.concurrent.TimeUnit;
 
 public class TextServiceImpl implements TextService {
-    
+    private final Source<Envelope, SourceQueueWithComplete<Envelope>> queueSource = Source.<Envelope>queue(10, OverflowStrategy.backpressure());
+    private final SourceQueueWithComplete<Envelope> queue;
+
     @Override
     public Topic<Envelope> textTopic() {
-        Source<Envelope, Cancellable> src = Source.tick(FiniteDuration.Zero(), FiniteDuration.apply(1000, TimeUnit.MILLISECONDS), Envelope.defaultInstance());
+        Source<Envelope, ?> src = queueSource; //Source.tick(FiniteDuration.Zero(), FiniteDuration.apply(1000, TimeUnit.MILLISECONDS), Envelope.defaultInstance());
+
         return TopicProducer.singleStreamWithOffset(offset -> src.map(e -> new Pair<>(e, offset)));
     }
 
     @Inject
     public TextServiceImpl(Materializer materializer, TweetImageService tweetImageService) {
+        this.queue = queueSource.to(Sink.ignore()).run(materializer);
         tweetImageService.tweetImageTopic().subscribe().withGroupId("text").atLeastOnce(Flow.fromFunction(this::extractText));
     }
 
     private Done extractText(Envelope envelope) {
         System.out.println("*** find text in " + envelope.messageId());
+        queue.offer(envelope);
         return Done.getInstance();
     }
 
